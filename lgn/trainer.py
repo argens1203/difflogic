00084f3a5bedf423516893f048c0c3f9b6daa85f
Argens <argens1203@gmail.com>
2025-03-06 13:16:14 +1100

    ref: cleaner eval

--- lgn/trainer.py
+++ lgn/trainer.py
@@ -1,9 +1,11 @@
-from constant import device, BITS_TO_TORCH_FLOATING_POINT_TYPE
-from tqdm import tqdm
 import torch
-from lgn.dataset import load_n
-from difflogic import PackBitsTensor
 import numpy as np
+from tqdm import tqdm
+
+from difflogic import PackBitsTensor
+
+from constant import device, BITS_TO_TORCH_FLOATING_POINT_TYPE
+from lgn.dataset import load_n
 
 
 def train(model, x, y, loss_fn, optimizer):
@@ -22,20 +24,7 @@ def eval(model, loader, mode):
         model.train(mode=mode)
         res = np.mean(
             [
-                (
-                    model(
-                        x.to(
-                            "cuda"
-                            if torch.cuda.is_available()
-                            else "mps" if torch.mps.is_available() else "cpu"
-                        ).round()
-                    ).argmax(-1)
-                    == y.to(
-                        "cuda"
-                        if torch.cuda.is_available()
-                        else "mps" if torch.mps.is_available() else "cpu"
-                    )
-                )
+                (model(x.to(device).round()).argmax(-1) == y.to(device))
                 .to(torch.float32)
                 .mean()
                 .item()
@@ -70,6 +59,62 @@ def packbits_eval(model, loader):
     return res.item()
 
 
+def multi_eval(
+    model,
+    train_loader,
+    validation_loader,
+    test_loader,
+    extensive_eval=True,
+    results=None,
+    packbits_eval=False,
+):
+    best_acc = 0
+
+    if extensive_eval:
+        # Use train mode to test on training data
+        train_accuracy_train_mode = eval(model, train_loader, mode=True)
+        valid_accuracy_eval_mode = eval(model, validation_loader, mode=False)
+        valid_accuracy_train_mode = eval(model, validation_loader, mode=True)
+    else:
+        train_accuracy_train_mode = -1
+        valid_accuracy_eval_mode = -1
+        valid_accuracy_train_mode = -1
+    # Use evaluation mode to test on training data
+    train_accuracy_eval_mode = eval(model, train_loader, mode=False)
+    test_accuracy_eval_mode = eval(model, test_loader, mode=False)
+    test_accuracy_train_mode = eval(model, test_loader, mode=True)
+
+    r = {
+        "train_acc_eval_mode": train_accuracy_eval_mode,
+        "train_acc_train_mode": train_accuracy_train_mode,
+        "valid_acc_eval_mode": valid_accuracy_eval_mode,
+        "valid_acc_train_mode": valid_accuracy_train_mode,
+        "test_acc_eval_mode": test_accuracy_eval_mode,
+        "test_acc_train_mode": test_accuracy_train_mode,
+    }
+
+    if packbits_eval:
+        pass
+        r["train_acc_eval"] = packbits_eval(model, train_loader)
+        r["valid_acc_eval"] = packbits_eval(model, train_loader)
+        r["test_acc_eval"] = packbits_eval(model, test_loader)
+
+    if results is not None:
+        results.store_results(r)
+    else:
+        print(r)
+
+    if valid_accuracy_eval_mode > best_acc:
+        best_acc = valid_accuracy_eval_mode
+        if results is not None:
+            results.store_final_results(r)
+        else:
+            print("IS THE BEST UNTIL NOW.")
+
+    if results is not None:
+        results.save()
+
+
 def train_eval(
     args,
     train_loader,
@@ -80,8 +125,6 @@ def train_eval(
     optim,
     results=None,
 ):
-    best_acc = 0
-
     for i, (x, y) in tqdm(
         enumerate(load_n(train_loader, args.num_iterations)),
         desc="iteration",
@@ -93,46 +136,12 @@ def train_eval(
         loss = train(model, x, y, loss_fn, optim)
 
         if (i + 1) % args.eval_freq == 0:
-            if args.extensive_eval:
-                # Use train mode to test on training data
-                train_accuracy_train_mode = eval(model, train_loader, mode=True)
-                valid_accuracy_eval_mode = eval(model, validation_loader, mode=False)
-                valid_accuracy_train_mode = eval(model, validation_loader, mode=True)
-            else:
-                train_accuracy_train_mode = -1
-                valid_accuracy_eval_mode = -1
-                valid_accuracy_train_mode = -1
-            # Use evaluation mode to test on training data
-            train_accuracy_eval_mode = eval(model, train_loader, mode=False)
-            test_accuracy_eval_mode = eval(model, test_loader, mode=False)
-            test_accuracy_train_mode = eval(model, test_loader, mode=True)
-
-            r = {
-                "train_acc_eval_mode": train_accuracy_eval_mode,
-                "train_acc_train_mode": train_accuracy_train_mode,
-                "valid_acc_eval_mode": valid_accuracy_eval_mode,
-                "valid_acc_train_mode": valid_accuracy_train_mode,
-                "test_acc_eval_mode": test_accuracy_eval_mode,
-                "test_acc_train_mode": test_accuracy_train_mode,
-            }
-
-            if args.packbits_eval:
-                pass
-                r["train_acc_eval"] = packbits_eval(model, train_loader)
-                r["valid_acc_eval"] = packbits_eval(model, train_loader)
-                r["test_acc_eval"] = packbits_eval(model, test_loader)
-
-            if args.experiment_id is not None:
-                results.store_results(r)
-            else:
-                print(r)
-
-            if valid_accuracy_eval_mode > best_acc:
-                best_acc = valid_accuracy_eval_mode
-                if args.experiment_id is not None:
-                    results.store_final_results(r)
-                else:
-                    print("IS THE BEST UNTIL NOW.")
-
-            if args.experiment_id is not None:
-                results.save()
+            multi_eval(
+                model,
+                train_loader,
+                validation_loader,
+                test_loader,
+                results=results,
+                packbits_eval=args.packbits_eval,
+                extensive_eval=args.extensive_eval,
+            )
