7bd64abcbdd7e2bfe06618ceac568ba8b2e9b01e
Argens <argens1203@gmail.com>
2025-04-08 22:37:21 +1000

    ref: Adult - use AutoTransformer

--- lgn/dataset/adult.py
+++ lgn/dataset/adult.py
@@ -1,12 +1,10 @@
 import numpy as np
 
 from .custom_dataset import CustomDataset
-
-from sklearn.preprocessing import LabelEncoder
-from .converter import Converter
+from .auto_transformer import AutoTransformer
 
 
-class AdultDataset(CustomDataset):
+class AdultDataset(CustomDataset, AutoTransformer):
     train_url, train_md5 = (
         "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data",
         "5d7c39d7b8804f071cdd1f2a7c460872",
@@ -42,6 +40,8 @@ class AdultDataset(CustomDataset):
         "capital-loss",
         "hours-per-week",
     }
+    # discrete_attributes would be replaced in AutoTransformer to be the rest of attributes
+    discrete_attributes = None
     bin_sizes = {
         "age": 5,
         "capital-gain": 3,
@@ -73,36 +73,5 @@ class AdultDataset(CustomDataset):
         # Remove fnlwgt and education-num
         features = np.delete(features, [2, 4], axis=1)
 
-        self.features = self.transform_feature(features)
-        self.labels = self.transform_label(labels)
-
-    def transform_label(self, labels):
-        if AdultDataset.label_encoder is None:
-            AdultDataset.label_encoder = LabelEncoder()
-            AdultDataset.label_encoder.fit(labels)
-
-        return AdultDataset.label_encoder.transform(labels)
-
-    def transform_feature(self, features):
-        if AdultDataset.converter is None:
-            AdultDataset.converter = Converter(
-                attributes=AdultDataset.attributes,
-                continuous_attributes=AdultDataset.continuous_attributes,
-                bin_sizes=AdultDataset.bin_sizes,
-            )
-            AdultDataset.converter.fit(features)
-
-        return AdultDataset.converter.transform(features)
-
-    def inverse_transform_feature(self, features):
-        return AdultDataset.converter.inverse_transform(features)
-
-    def inverse_transform_label(self, labels):
-        return AdultDataset.label_encoder.inverse_transform(labels)
-
-    # -- Getters -- #
-    def get_attribute_ranges():
-        return AdultDataset.converter.get_n_classes()
-
-    def get_input_dim():
-        return sum(AdultDataset.converter.get_n_classes())
+        self.features = AdultDataset.transform_feature(features)
+        self.labels = AdultDataset.transform_label(labels)
