16a7e4ce0f75d7f92016dba54f8f59c80f4d8d52
Argens <argens1203@gmail.com>
2025-04-08 22:23:23 +1000

    ref: use auto_transformer in monk

--- lgn/dataset/monk.py
+++ lgn/dataset/monk.py
@@ -1,11 +1,13 @@
 from .custom_dataset import CustomDataset
-from .converter import Converter
-from sklearn.preprocessing import LabelEncoder
+from .auto_transformer import Autotransformer
 
 
-class MonkDataset(CustomDataset):
+class MonkDataset(CustomDataset, Autotransformer):
     attributes = [f"attribute_{i}" for i in range(1, 7)]
-    continuous_attributes = set()
+    continuous_attributes = set()  # No continuous attributes in Monk datasets
+    bin_sizes = dict()  # No bin sizes since no continuous attributes
+
+    discrete_attributes = set(attributes)
     converter = None
     label_encoder = None
 
@@ -20,38 +22,8 @@ class MonkDataset(CustomDataset):
         raw_data = raw_data[:, :-1]  # Remove instance id
         labels, features = raw_data[:, 0], raw_data[:, 1:]
 
-        self.features = self.transform_feature(features)
-        self.labels = self.transform_label(labels)
-
-    def transform_label(self, labels):
-        if MonkDataset.label_encoder is None:
-            MonkDataset.label_encoder = LabelEncoder()
-            MonkDataset.label_encoder.fit(labels)
-
-        return MonkDataset.label_encoder.transform(labels)
-
-    def transform_feature(self, features):
-        if MonkDataset.converter is None:
-            MonkDataset.converter = Converter(
-                attributes=MonkDataset.attributes,
-                continuous_attributes=MonkDataset.continuous_attributes,
-            )
-            MonkDataset.converter.fit(features)
-
-        return MonkDataset.converter.transform(features)
-
-    def inverse_transform_feature(self, features):
-        return MonkDataset.converter.inverse_transform(features)
-
-    def inverse_transform_label(self, labels):
-        return MonkDataset.label_encoder.inverse_transform(labels)
-
-    # -- Getters -- #
-    def get_attribute_ranges():
-        return MonkDataset.converter.get_n_classes()
-
-    def get_input_dim():
-        return sum(MonkDataset.converter.get_n_classes())
+        self.features = MonkDataset.transform_feature(features)
+        self.labels = MonkDataset.transform_label(labels)
 
 
 class Monk1Dataset(MonkDataset):
